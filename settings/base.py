0bc2d624b05bc8524d86be3d25a0eae53b04de79
Ayush Garg <gargayush341@gmail.com>
2024-02-16 08:24:38 +0530

    changes for prod ready deploy

--- settings/base.py
+++ settings/base.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-6&cs9)^r192wby)r7ngt%_juq+vm0qyp7nqh2j2luev2rvnng+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["rentdoor.in", "localhost", "127.0.0.1"]
 
 
 # Application definition
--- settings/prod.py
+++ settings/prod.py
@@ -1,1 +1,6 @@
 from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ["rentdoor.in", "localhost", "127.0.0.1"]
