0451cfc3d5ec261855d0690dc467136742843760
Ayush Garg <gargayush341@gmail.com>
2024-02-24 17:26:27 +0530

    newrelic key substituted from env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Collect static files
 echo "Collect static files"
-python manage.py collectstatic --no-input
+"yes" | python manage.py collectstatic
 
 # Apply database migrations if any
 echo "Apply database migrations"
@@ -11,6 +11,9 @@ echo "Apply database migrations"
 # Start server
 echo "Starting gunicorn server"
 echo "env variable:- $ENV"
+
+sed -i 's/new_relic_license_key/'$NEW_RELIC_LICENSE_KEY'/g' newrelic.ini
+
 if [ "$ENV" == "prod" ]; then
     NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn --preload -c python:settings.gunicorn rentdoor.wsgi
 else
--- settings/prod.py
+++ settings/prod.py
@@ -3,7 +3,13 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["housedoor.in", "localhost", "127.0.0.1", "https://housedoor.in"]
+ALLOWED_HOSTS = [
+    "housedoor.in",
+    "localhost",
+    "127.0.0.1",
+    "https://housedoor.in",
+    "139.59.17.7",
+]
 
 CSRF_TRUSTED_ORIGINS = ["https://housedoor.in"]
 
